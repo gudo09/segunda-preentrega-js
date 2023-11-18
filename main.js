5d840f7d1a868f4668239045a30362e062841fe0
Franco Gudiño <francogudino12@gmail.com>
2023-11-18 17:14:43 -0300

    Agrego las consignas como comentarios en el proyecto

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+Entregar:
+  -Estructura HTML
+  -Variables
+  -Funciones
+  -Objetos (literales o funcion constuctora)
+  -Arrays (y sus metodos especialmente busqueda y filtrado)
+  -Metodos de filtrado
+  -Iteracion (algunas de las variantes del FOR)
+*/
